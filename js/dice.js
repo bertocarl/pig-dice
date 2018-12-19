4c8561a36b20d3e9eb32837fe4596076f386f9fc
Albert Omware <aomware@gmail.com>
2018-12-19 16:53:27 +0300

    added code for rolling the dice

--- js/dice.js
+++ js/dice.js
@@ -1,15 +1,24 @@
-var [scrore1,score0,current0,current1,totalscore,rndscore]=0
+// var [scrore1,score0,current0,current1,totalscore,rndscore]=0
 // New game button
 $(document).ready(function() {
   $(".btnNew").click(function() {
     reset();
 // Roll the dice
-$(".btnRoll").click(function() {
-  roll ();
+$(document).ready(function() {
+$("#btnRoll").click(function() {
+
+  var current = 0;
+  var rollDice = Math.floor(Math.random() * 6)+1;
+
+  if(rollDice === 1) {
+    alert("You just rolled a 1. pass dice to next player")
+  } else {
+    current+=rollDice;
+
+  }
+  $('#current0').text(current + rollDice);
+});
+});
 // Hold the dice
 $(".btnHold").click(function() {
   hold ();
-function roll () {
-
-
-}
